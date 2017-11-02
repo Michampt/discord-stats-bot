e154e66380e758255be85e58a8c9f3381fe22828
Michael <hamptonm88@gmail.com>
2017-11-02 00:14:52 -0700

    Added the destiny2 cog

--- bot.py
+++ bot.py
@@ -1,12 +1,13 @@
-import discord, sqlite3
+import discord, sqlite3, logging
 from discord.ext.commands import Bot
 import secrets
 
 statbot = Bot(command_prefix='!')
-startup_extensions = ['cogs.pubg', 'cogs.wow', 'cogs.d3', 'cogs.info']
+startup_extensions = ['cogs.pubg', 'cogs.wow', 'cogs.d3', 'cogs.info', 'cogs.destiny2']
 
 conn = sqlite3.connect('db/statbot.db')
 cur = conn.cursor()
+logging.basicConfig(level=logging.INFO)
 
 
 @statbot.event
